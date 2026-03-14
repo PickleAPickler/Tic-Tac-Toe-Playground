629beb032347d05313c981fbe1e9efcc1ca7adc9
Alan <a.nicholson2358@gmail.com>
2026-03-14 16:26:13 +0000

    Adding unittest script

---
+++ TTT_Unittests.py
@@ -0,0 +1,16 @@
+
+
+
+import unittest
+import os
+
+
+#region UNITTEST ODDESSY
+# https://docs.python.org/3/library/unittest.html#unittest.TestCase:~:text=debugger%2E-,The,manager
+
+# class TestTTT(unittest.TestCase):
+
+#     def test_displayB(self):
+        # TBC...
+
+#endregion UNITTEST ODDESSY
